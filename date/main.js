802297574ace8015c46da00e2ba48f635cd4f43f
m25563017 <71679886+m25563017@users.noreply.github.com>
2023-02-19 21:20:18 +0800

    html5的練習

---
+++ date/main.js
@@ -0,0 +1,28 @@
+var ctx, thisImage;
+$(function () {
+    $("[type='date']").on("change", showDate);
+    ctx = $("#myCanvas")[0].getContext("2d");
+});
+function showDate(){
+    var thisDate = this.value;
+    thisDate = thisDate.replace(/-/g,"");
+    thisImage = new Image();
+    thisImage.src = "flipClockNumbers.png";
+    thisImage.onload = function () {
+        for (var x = 0;
+            x < thisDate.length; x++) {
+            ctx.drawImage(thisImage, thisDate[x] * 80, 0, 90, 130, 60 * x, 0, 60, 100);
+        }
+    };
+}
+
+
+$(function (){
+    $("[type='color']").on("change",changBGC);
+});
+
+function changBGC(){
+    console.log(this.value);
+    $('body').css("background-color",this.value);
+}
+
